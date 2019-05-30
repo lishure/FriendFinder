307ccdbb6a11b690b1b276b4800ccd9b583f9316
Lishure <HUEY_LISA@YAHOO.COM>
2019-05-29 18:47:33 -0700

    added href route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,9 @@
 var friends = require("../data/friends");
 module.exports = function (app) {
-    app.get("/api/friends", function (req, res) {
+    app.get("/friends", function (req, res) {
         res.json(friends);
     })
-    app.post("/api/friends", function (req, res) {
+    app.post("/friends", function (req, res) {
         var totalDifference = 0;
         var bestMatch = {
             name: "",

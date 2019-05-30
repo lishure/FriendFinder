020408199ae320b5b88707f7e323ce14a79edf50
Lishure <HUEY_LISA@YAHOO.COM>
2019-05-29 21:11:25 -0700

    Added images for friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
 {
-    "name":"Prince",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Ross",
+    "photo":"https://pbs.twimg.com/profile_images/1017221140677349376/dhSnvw-1_400x400.jpg",
     "scores":[
         5,
         1,
@@ -17,7 +17,7 @@ var friends = [
   },
   {
     "name":"Joey",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://www.lifeandstylemag.com/wp-content/uploads/2017/11/joey-tribbiani-thanksgiving-friends.png?fit=200%2C1",
     "scores":[
         5,
         5,
@@ -32,8 +32,8 @@ var friends = [
       ]
   },
   {
-    "name":"Lucky",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Rachel",
+    "photo":"https://www.tvovermind.com/wp-content/uploads/2018/04/7c5320c90e618031de6f7b17cffbc13b-640x427.jpg",
     "scores":[
         5,
         1,

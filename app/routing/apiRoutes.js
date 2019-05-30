a67ba9293a4393cb87fb60dfb148b0f1b9ba0088
Lishure <HUEY_LISA@YAHOO.COM>
2019-05-29 21:26:50 -0700

    cleaned up code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,11 @@
 var friends = require("../data/friends");
 
 module.exports = function(app) {
+    //get data from friends file
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
-
+//post data to friends file
   app.post("/api/friends", function(req, res) {
     var totalDifference = 0;
 
@@ -13,7 +14,7 @@ module.exports = function(app) {
       photo: "",
       friendDifference: 1000
     };
-
+//store user data in variable
     var userData = req.body;
     var userName = userData.name;
     var userScores = userData.scores;
@@ -29,24 +30,24 @@ module.exports = function(app) {
 
     console.log("Name: " + userName);
     console.log("User Score " + userScores);
-
+//sum of score differences
     var sum = b.reduce((a, b) => a + b, 0);
 
     console.log("Sum of users score " + sum);
     console.log("Best match friend diff " + bestMatch.friendDifference);
     console.log("+++++++=================++++++++++");
-
+//for loop for friends array for friend score difference
     for (var i = 0; i < friends.length; i++) {
       console.log(friends[i].name);
       totalDifference = 0;
       console.log("Total Diff " + totalDifference);
       console.log("Best match friend diff " + bestMatch.friendDifference);
-
+//best friend match score difference
       var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
       console.log("Total friend score " + bfriendScore);
       totalDifference += Math.abs(sum - bfriendScore);
       console.log("-------------------------> " + totalDifference);
-
+//if statement for best friend match  to show
       if (totalDifference <= bestMatch.friendDifference) {
         bestMatch.name = friends[i].name;
         bestMatch.photo = friends[i].photo;
@@ -55,7 +56,7 @@ module.exports = function(app) {
       console.log(totalDifference + " Total Difference");
     }
     console.log(bestMatch);
-
+//user data to push to friends array
     friends.push(userData);
     console.log("New user added");
     console.log(userData);

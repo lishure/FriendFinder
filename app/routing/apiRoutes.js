f31784c1209ab7363114ea1b9c239af71340d02d
Lishure <HUEY_LISA@YAHOO.COM>
2019-05-29 20:03:26 -0700

    fixed route & working click button

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,66 +1,64 @@
 var friends = require("../data/friends");
-module.exports = function (app) {
-    app.get("/friends", function (req, res) {
-        res.json(friends);
-    })
-    app.post("/friends", function (req, res) {
-        var totalDifference = 0;
-        var bestMatch = {
-            name: "",
-            photo: "",
-            friendDifference: 1000
-        };
-        var newFriend = req.body;
-        var friendName = newFriend.name;
-        var friendScores = newFriend.scores;
-        //The parseInt makes sure that the score is a number and not a string
-        var scoreNumber = friendScores.map(function(item) {
-            return parseInt(item, 10);
-        }); 
-        newFriend = {
-            name: req.body.name,
-            photo: req.body.photo,
-            scores: scoreNumber
-        };
-        console.log("Name: ", friendName);
-        console.log("User score: ", friendScores);
 
-        var sum =scoreNumber.reduce((a, b)=> a + b, 0);
-        console.log("Sum of friend score ", sum);
-        console.log("Best match friend diff ", bestmatch.friendDifference);
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
 
+  app.post("/api/friends", function(req, res) {
+    var totalDifference = 0;
 
-        for (var i=0; i<friends.length; i++){
-            console.log(friends[i].name);
-            totalDifference = 0;
-            console.log("Total diff", totalDifference);
-            console.log("Best match friend diff", bestMatch.friendDifference);
-         
-            var bestmatchScore = friends[i].scores.reduce((a, b) => a + b, 0);
-            console.log("Total friend score " + bestmatchScore);
-            totalDifference += Math.abs(sum - bestmatchScore);
-            console.log("results " +totalDifference);
-            // var currentFriend =friends[i];
-            // //console.log(currentFriend.scores)
-            // for (var j=0; j<currentFriend.scores.length; j++) {
-            //     console.log(newFriend.name, newFriend.scores[j])
-            //     console.log(currentFriend.name,currentFriend.scores[j])
-            // }
-if (totalDifference <= bestMatch.friendDifference) {
-    bestMatch.name = friends[i].name;
-    bestMatch.photo = friends[i].photo;
-    bestMatch.friendDifference = totalDifference;
-}
-    console.log(totalDifference + "Total Difference");
-}
-console.log(bestMatch);
-friends.push(friendData);
-console.log("New Friend added");
-console.log(friendData);
-res.json(bestMatch);
-});
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
 
+    var userData = req.body;
+    var userName = userData.name;
+    var userScores = userData.scores;
 
-       // res.send("Match Found")
-    
+    var b = userScores.map(function(item) {
+      return parseInt(item, 10);
+    });
+    userData = {
+      name: req.body.name,
+      photo: req.body.photo,
+      scores: b
+    };
+
+    console.log("Name: " + userName);
+    console.log("User Score " + userScores);
+
+    var sum = b.reduce((a, b) => a + b, 0);
+
+    console.log("Sum of users score " + sum);
+    console.log("Best match friend diff " + bestMatch.friendDifference);
+    console.log("+++++++=================++++++++++");
+
+    for (var i = 0; i < friends.length; i++) {
+      console.log(friends[i].name);
+      totalDifference = 0;
+      console.log("Total Diff " + totalDifference);
+      console.log("Best match friend diff " + bestMatch.friendDifference);
+
+      var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
+      console.log("Total friend score " + bfriendScore);
+      totalDifference += Math.abs(sum - bfriendScore);
+      console.log("-------------------------> " + totalDifference);
+
+      if (totalDifference <= bestMatch.friendDifference) {
+        bestMatch.name = friends[i].name;
+        bestMatch.photo = friends[i].photo;
+        bestMatch.friendDifference = totalDifference;
+      }
+      console.log(totalDifference + " Total Difference");
+    }
+    console.log(bestMatch);
+
+    friends.push(userData);
+    console.log("New user added");
+    console.log(userData);
+    res.json(bestMatch);
+  });
 };
--- server.js
+++ server.js
@@ -1,25 +1,19 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-// Link in html and api routes
-var apiRoutes = require('./app/routing/apiRoutes.js');
-var htmlRoutes = require('./app/routing/htmlRoutes.js');
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
-// Set up Express App
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-//app.use(express.static('app/public'));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+app.use(express.static("app/public"));
 
-// // Server Routing Map 
- apiRoutes(app); // API route - Must be listed first due to the HTML default catch all "use" route
- htmlRoutes(app); // HTML route 
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 // Listener - Start the server
 app.listen(PORT, function() {
